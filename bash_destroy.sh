9565a936b6ce0f4f5cfa7e8ba11a377bbd5e85b5
Abdul Rehman <i192190@lhr.nu.edu.pk>
2023-07-17 16:38:47 +0500

    m

--- bash_destroy.sh
+++ bash_destroy.sh
@@ -1,6 +1,7 @@
 # Move into the cloned directory
 echo "Moving into the cloned directory..."
-cd Terraform-EC2-RDS
+cd Terraform-with-ansible
+cd Terraform-scripts
 
 terraform destroy -auto-approve
 
